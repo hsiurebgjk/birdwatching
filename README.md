41aefbfc477283baa7c04fdcede226246c0c95c2
Julian <adler.julian@proton.me>
2026-02-10 21:54:00 +1300

    starting the service is always better

--- README.md
+++ README.md
@@ -2,41 +2,6 @@
 
 Website to list the bird we saw!
 
-All sizes are in centimetres, heights in grams.
+All sizes are in centimeters, weights in grams.
 
-# Rsbuild project
-
-## Setup
-
-Install the dependencies:
-
-```bash
-npm install
-```
-
-## Get started
-
-Start the dev server, and the app will be available at [http://localhost:3000](http://localhost:3000).
-
-```bash
-npm run dev
-```
-
-Build the app for production:
-
-```bash
-npm run build
-```
-
-Preview the production build locally:
-
-```bash
-npm run preview
-```
-
-## Learn more
-
-To learn more about Rsbuild, check out the following resources:
-
-- [Rsbuild documentation](https://rsbuild.rs) - explore Rsbuild features and APIs.
-- [Rsbuild GitHub repository](https://github.com/web-infra-dev/rsbuild) - your feedback and contributions are welcome!
+Browse [here](https://hsiurebgjk.github.io/birdwatching/ 'gh-pages link')
--- src/App.jsx
+++ src/App.jsx
@@ -12,73 +12,76 @@ const App = () => {
   const sort = (property) => setBirds(sortJsonObject(birds, property));
 
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Links</th>
-          <th onClick={() => sort('teReoName')}>Te Reo Name</th>
-          <th onClick={() => sort('name')}>Name</th>
-          <th onClick={() => sort('rarity')}>Rarity</th>
-          <th onClick={() => sort('origin')}>Origin</th>
-          <th>Size</th>
-          <th>Weight</th>
-          <th onClick={() => sort('rating')}>Rating</th>
-          <th onClick={() => sort('location')}>Where</th>
-          <th onClick={() => sort('comment')}>Comment</th>
-        </tr>
-      </thead>
-      <tbody>
-        {ready &&
-          birds &&
-          birds.map(
-            (
-              {
-                name,
-                nzBirdName,
-                scientificName,
-                teReoName,
-                origin,
-                rarity,
-                rating,
-                comment,
-                size,
-                weight,
-                location,
+    <>
+      <table>
+        <thead>
+          <tr>
+            <th>Links</th>
+            <th onClick={() => sort('teReoName')}>Te Reo Name</th>
+            <th onClick={() => sort('name')}>Name</th>
+            <th onClick={() => sort('rarity')}>Rarity</th>
+            <th onClick={() => sort('origin')}>Origin</th>
+            <th>Size</th>
+            <th>Weight</th>
+            <th onClick={() => sort('rating')}>Rating</th>
+            <th onClick={() => sort('location')}>Where</th>
+            <th onClick={() => sort('comment')}>Comment</th>
+          </tr>
+        </thead>
+        <tbody>
+          {ready &&
+            birds &&
+            birds.map(
+              (
+                {
+                  name,
+                  nzBirdName,
+                  scientificName,
+                  teReoName,
+                  origin,
+                  rarity,
+                  rating,
+                  comment,
+                  size,
+                  weight,
+                  location,
+                },
+                index,
+              ) => {
+                return (
+                  <tr key={index}>
+                    <td>
+                      {<DuckLink name={name} />}
+                      &nbsp;
+                      {<NzBirdOnline nzBirdName={nzBirdName} name={name} />}
+                    </td>
+                    <td>{teReoName}</td>
+                    <td>
+                      {name}
+                      &nbsp;
+                      <a title={scientificName}>&#9432;</a>
+                    </td>
+                    <td>{rarity}</td>
+                    <td>{origin}</td>
+                    <td>{<Size value={size} unit="cm" />}</td>
+                    <td>{<Size value={weight} unit="g" />}</td>
+                    <td>
+                      {[...new Array(rating)].map((_) => '★').join('') || '-'}
+                    </td>
+                    <td>
+                      {location || 'Not yet :('}
+                      &nbsp;
+                      <OsmLink location={location} />
+                    </td>
+                    <td>{comment}</td>
+                  </tr>
+                );
               },
-              index,
-            ) => {
-              return (
-                <tr key={index}>
-                  <td>
-                    {<DuckLink name={name} />}
-                    &nbsp;
-                    {<NzBirdOnline nzBirdName={nzBirdName} name={name} />}
-                  </td>
-                  <td>{teReoName}</td>
-                  <td>
-                    {name}
-                    &nbsp;
-                    <a title={scientificName}>&#9432;</a>
-                  </td>
-                  <td>{rarity}</td>
-                  <td>{origin}</td>
-                  <td>{<Size value={size} unit="cm" />}</td>
-                  <td>{<Size value={weight} unit="g" />}</td>
-                  <td>
-                    {[...new Array(rating)].map((_) => '★').join('') || '-'}
-                  </td>
-                  <td>
-                    {location || 'Not yet :('}
-                    &nbsp;
-                    <OsmLink location={location} />
-                  </td>
-                  <td>{comment}</td>
-                </tr>
-              );
-            },
-          )}
-      </tbody>
-    </table>
+            )}
+        </tbody>
+      </table>
+      <a href="https://github.com/hsiurebgjk/birdwatching">Sources</a>
+    </>
   );
 };
 
